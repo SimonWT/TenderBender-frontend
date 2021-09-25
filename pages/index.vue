74291360b88e0892a1394794b7814ee0cda74d38
yaroslav_yudi <yaskih@mail.ru>
2021-09-25 12:27:32 +0400

    init

---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="app">
+    <vs-button> Hello World Vuesax + Nuxtjs </vs-button>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+.app {
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
